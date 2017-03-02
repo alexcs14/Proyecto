7b4d14b2a3dfbbbc63b4c9274a0a3866adb58f09
yenilpz <shatanoga.yp@gmail.com>
2017-03-02 10:29:21 -0500

    model y controller de donacion
    falta hacer consultar

---
+++ controller/donacion.controller.php
@@ -0,0 +1,62 @@
+<?php
+ require_once("../model/conn.model.php");
+ require_once("../model/donacion.class.php");
+
+$action=$_REQUES["action"];
+
+switch ($accion){
+  case 'A':
+  $don_cod=$_POST["Codigo"];
+  $don_usu_cod=$_POST["Codigo de suario"];
+  $don_td_cod=$_POST["Tipo de donacion"];
+  $don_fecha_res=$_POST["Fecha de reserva"];
+  $don_fecha_cita=$_POST["Fecha de citacion"];
+  $don_lugar=$_POST["lugar de citacion"];
+
+  if($don_cod=="" or $don_usu_cod=="" or $don_td_cod=="" or  $don_fecha_res=="" or $don_fecha_cita=="" or $don_lugar==""){
+    echo "Debes llanar todos los campos para continuar,Por favor verificarlos. ";
+  }else{
+    try {
+      Gestionar_donacion::Crear( $don_cod,$don_usu_cod  $don_td_cod, $don_fecha_res, $don_fecha_cita, $don_lugar);
+    }catch(Exception $e){
+      echo $e;
+    }
+  }
+  break;
+  case 'B'
+
+
+  break;
+  case 'C':
+  $don_cod=$_POST["codigo"];
+  $don_usu_cod=$_POST["Codigo de suario"];
+  $don_td_cod=$_POST["Tipo de donacion"];
+  $don_fecha_res=$_POST["Fecha de reserva"];
+  $don_fecha_cita=$_POST["Fecha de citacion"];
+  $don_lugar=$_POST["lugar de citacion"];
+    try {
+      Gestionar_donacion::Modificar($don_cod ,$don_usu_cod,$don_td_cod ,$don_fecha_res ,$don_fecha_cita , $don_lugar);
+    } catch (Exception $e) {
+      echo $e;
+    }s
+  break;
+  case 'D':
+  // 	echo "
+  // 	<script>
+  // 		confirm('desea eliminar');
+  // 		if (confirm) {".
+  //
+  // 		"}
+  // 	</script>";
+  $don_cod=$_POST["codigo"];
+  try {
+    Gestionar_donacion::Eliminar($don_cod ,$don_usu_cod,$don_td_cod ,$don_fecha_res ,$don_fecha_cita , $don_lugar);
+    echo 'Eliminado con exito';
+  } catch (Exception $e) {
+    echo $e;
+  }
+
+  break;
+}
+header("location: ../views/module/sdonacion.php");
+ ?>
